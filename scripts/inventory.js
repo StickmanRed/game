9aa3178a3a2e22cad216e26eb41478968457f2d4
StickmanRed <100049599+StickmanRed@users.noreply.github.com>
2024-06-28 23:52:58 -0700

    Update inventory.js

--- scripts/inventory.js
+++ scripts/inventory.js
@@ -1,6 +1,5 @@
 /* Formats the inventory and appends it to the INVENTORY section */
 function inventoryHTML(name, amount){
-    // Gotta be careful with these... 
     return `<div class="inv-item"><span class="inv-item-name">${name.toUpperCase().replace(/[^\w]+/g, "")} - </span><span class="inv-item-amount">${(typeof amount === "number") ? amount : 0}</span></div>`;
 }
 
