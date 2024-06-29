d0cc99826774e781e81866f49225af4b72437afe
StickmanRed <100049599+StickmanRed@users.noreply.github.com>
2024-06-28 23:43:55 -0700

    Update inventory.js

--- scripts/inventory.js
+++ scripts/inventory.js
@@ -1,16 +1,13 @@
 /* Formats the inventory and appends it to the INVENTORY section */
-import data from "../data/save.json" with {type: "json"};
-console.log(data);
-
-function returnInventoryContent(name, amount){
+function inventoryHTML(name, amount){
     // Gotta be careful with these... 
     return `<div class="inv-item"><span class="inv-item-name">${name.toUpperCase().replace(/[^\w]+/g, "")} - </span><span class="inv-item-amount">${(typeof amount === "number") ? amount : 0}</span></div>`;
 }
 
-export default function renderInventory(){
+export default function renderInventory(data) {
     let newHTML = "";
     data.inventory.forEach((item) => {
-        newHTML += returnInventoryContent(item[0], item[1]);
+        newHTML += inventoryHTML(item[0], item[1]);
     });
     $("#inv-contents").append(newHTML);
 }
